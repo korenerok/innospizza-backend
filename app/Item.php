e97daa527ef65bd9cfe83baf52ecc4ea84b78c24
korenerok <marvin.bernal@gmx.com>
2020-05-04 15:36:33 -0400

    -Modified Model to hide 'item_category' field in responses. -Added README.md

--- app/Item.php
+++ app/Item.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    protected $hidden=['created_at','updated_at'];
+    protected $hidden=['created_at','updated_at','item_category'];
 
     public function toppings(){
         return $this->belongsToMany('App\Topping','pizza_topping','pizza_id','topping_id');
